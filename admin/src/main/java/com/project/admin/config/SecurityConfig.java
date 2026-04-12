4a056444e141314a298d62c69d75af64d92e85b9
이기석 <rltjr147@gmail.com>
2026-04-12 19:33:02 +0900

    [refactor] 중복 설정 제거

--- admin/src/main/java/com/project/admin/config/SecurityConfig.java
+++ admin/src/main/java/com/project/admin/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.admin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,13 +18,21 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/admin/signup", "/api/admin/login").permitAll()
-                        .anyRequest().authenticated());
-        return  http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/admin/signup", "/admin/signin").permitAll()
+                                .requestMatchers("/admin/**").authenticated()
+                ).
+                formLogin(form ->
+                        form.loginProcessingUrl("/admin/login")
+                                .loginPage("/admin/login"))
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                );
+
+        return http.build();
     }
 
 }
