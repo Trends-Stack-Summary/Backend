264b4a160fd3fc4f359bafdce631f784cdcbc4d1
이기석 <rltjr147@gmail.com>
2026-04-12 19:31:54 +0900

    [refactor] sign 경로 변경

--- admin/src/main/java/com/project/admin/domain/controller/AdminController.java
+++ admin/src/main/java/com/project/admin/domain/controller/AdminController.java
@@ -1,52 +1,44 @@
 package com.project.admin.domain.controller;
 
-import com.project.admin.domain.dto.login.AdminLoginResult;
 import com.project.admin.domain.dto.login.LoginResponse;
-import com.project.admin.domain.dto.login.AdminLoginCommand;
 import com.project.admin.domain.dto.login.AdminLoginRequest;
-import com.project.admin.domain.dto.signup.AdminSignupCommand;
 import com.project.admin.domain.dto.signup.AdminSignupRequest;
 import com.project.admin.domain.service.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
-@Slf4j
 public class AdminController {
 
     private final AdminService adminService;
 
 
     @PostMapping("/admin/signup")
-    private ResponseEntity<String> signup(@Valid @RequestBody AdminSignupRequest request) {
+    public ResponseEntity<String> signup(@Valid @RequestBody AdminSignupRequest request) {
 
-        adminService.createAdmin(AdminSignupCommand.from(request));
+        adminService.createAdmin(request.name(), request.loginId(), request.password());
 
         return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 완료");
 
     }
 
 
-    @PostMapping("/admin/login")
+    @PostMapping("/admin/signin")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody AdminLoginRequest loginRequest, HttpServletRequest request) {
 
-        AdminLoginResult loginAdmin = adminService.loginAdmin(AdminLoginCommand.from(loginRequest));
+        LoginResponse loginAdmin = adminService.loginAdmin(loginRequest.loginId(), loginRequest.password());
 
         HttpSession session = request.getSession();
-        session.setAttribute("admin", loginAdmin.id());
+        session.setAttribute("admin", loginAdmin.adminId());
 
 
-        log.info("session={}", session);
-
-        return ResponseEntity.ok(LoginResponse.from(loginAdmin));
+        return ResponseEntity.ok(loginAdmin);
 
     }
 }
