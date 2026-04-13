c52bb604c288640c2a1340215424baa623b7856a
2giseok <rltjr147@gmail.com>
2026-04-13 22:54:05 +0900

    [refactor] 세션 관리 제거 및 login 경로 변경

--- admin/src/main/java/com/project/admin/domain/controller/AdminController.java
+++ admin/src/main/java/com/project/admin/domain/controller/AdminController.java
@@ -1,11 +1,9 @@
 package com.project.admin.domain.controller;
 
-import com.project.admin.domain.dto.login.LoginResponse;
-import com.project.admin.domain.dto.login.AdminLoginRequest;
-import com.project.admin.domain.dto.signup.AdminSignupRequest;
+import com.project.admin.dto.AdminSigninResponse;
+import com.project.admin.dto.AdminSigninRequest;
+import com.project.admin.dto.AdminSignupRequest;
 import com.project.admin.domain.service.AdminService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -30,12 +28,9 @@ public class AdminController {
 
 
     @PostMapping("/admin/signin")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody AdminLoginRequest loginRequest, HttpServletRequest request) {
+    public ResponseEntity<AdminSigninResponse> signin(@Valid @RequestBody AdminSigninRequest loginRequest) {
 
-        LoginResponse loginAdmin = adminService.loginAdmin(loginRequest.loginId(), loginRequest.password());
-
-        HttpSession session = request.getSession();
-        session.setAttribute("admin", loginAdmin.adminId());
+        AdminSigninResponse loginAdmin = adminService.adminSignin(loginRequest.loginId(), loginRequest.password());
 
 
         return ResponseEntity.ok(loginAdmin);
