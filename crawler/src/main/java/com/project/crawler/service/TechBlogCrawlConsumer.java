0c220279a7c650fa6f7838f426a2d258c51655d0
2giseok <rltjr147@gmail.com>
2026-07-08 21:51:15 +0900

    [refactor] code assist 반영

--- crawler/src/main/java/com/project/crawler/service/TechBlogCrawlConsumer.java
+++ crawler/src/main/java/com/project/crawler/service/TechBlogCrawlConsumer.java
@@ -2,6 +2,9 @@ package com.project.crawler.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +15,15 @@ public class TechBlogCrawlConsumer {
 
     private final CrawlStorageService crawlStorageService;
 
-    @RabbitListener(queues = "crawlerQueue")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "crawlerQueue", durable = "true"),
+            exchange = @Exchange(value = "crawlerExchange", type = "direct"),
+            key = "crawlRouting"
+    ))
     public void consumerCrawl(String url) {
         log.info("RabbitMQ 시작, url={}", url);
 
         crawlStorageService.crawl(url);
-        log.info("크롤링 성공  url={}", url);
-
     }
 
 }
