e29c6e6d0ae9c2a9d58ece5e7a3cfec8865afae3
2giseok <rltjr147@gmail.com>
2026-07-22 17:28:52 +0900

    [refactor] EnableResilientMethods 추가

--- admin/src/main/java/com/project/admin/AdminApplication.java
+++ admin/src/main/java/com/project/admin/AdminApplication.java
@@ -3,11 +3,11 @@ package com.project.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
+@EnableResilientMethods
 public class AdminApplication {
 
     public static void main(String[] args) {
